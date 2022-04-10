5b8cd4c703b65b92a0486f42fcd10a6c52ca27b0
Nicholas <mullally.nt@gmail.com>
2022-04-10 16:29:40 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//I used the slice method to capitalize each element of the tutorials array. 
+
+/*
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -14,3 +18,125 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+
+const str1 = "what does this keyword mean?";
+const arr1 = str1.split(" ");
+for (var i = 0; i < arr.length; i++) {
+  arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+}
+const str2 = arr.join(" ");
+console.log(str2);
+
+*/
+
+/*const katzDeli = ["Ada", "Grace", "Kent", "Matz"];
+function line(array) {
+ for (i = 0; i < array.length; i--) {
+  result = "The line is currently: 1. " + katzDeli[0] + " 2. " + katzDeli[1] + " 3. " + katzDeli[2] + " 4. " + katzDeli[3]
+  return result;}
+}
+console.log(line(katzDeli)) 
+
+
+/*const katzDeli = []
+function nowServing(array) {
+ 
+  result = katzDeli.shift;
+  return result;
+    if (result === undefined); {
+     console.log("There is nobody waiting to be served!")
+  } 
+   }
+console.log("Currently serving " + katzDeli.shift()) 
+console.log(nowServing(katzDeli))
+
+/*const katzDeli = ["Ada", "Grace", "Kent", "Matz"];
+function takeANumber(array, str) {
+  result = array.pop(str);
+  return result;
+}
+takeANumber(katzDeli, "Matz")
+console.log("Welcome, " + result + ". You are number " + (katzDeli.length + 1) + " in line.")*/
+
+const str1 = "what does this keyword mean?";
+const arr1 = str1.split(" ");
+for (var i = 0; i < arr1.length; i++) {
+  arr1[i] = arr1[i].charAt(0).toUpperCase() + arr1[i].slice(1);
+}
+const str2 = arr1.join(" ");
+console.log(str2);
+
+const str3 = "What is the Constructor OO pattern?";
+const arr2 = str3.split(" ");
+for (var i = 0; i < arr2.length; i++) {
+  arr2[i] = arr2[i].charAt(0).toUpperCase() + arr2[i].slice(1);
+}
+const str4 = arr2.join(" ");
+console.log(str4);
+
+const str5 = "implementing Blockchain Web API";
+const arr3 = str3.split(" ");
+for (var i = 0; i < arr2.length; i++) {
+  arr2[i] = arr2[i].charAt(0).toUpperCase() + arr2[i].slice(1);
+}
+const str6 = arr2.join(" ");
+console.log(str6);
+
+const str7 =  "The Test Driven Development Workflow";
+const arr4 = str7.split(" ");
+for (var i = 0; i < arr4.length; i++) {
+  arr4[i] = arr4[i].charAt(0).toUpperCase() + arr4[i].slice(1);
+}
+const str8 = arr4.join(" ");
+console.log(str8);
+
+
+
+const str9 = "What is NaN and how Can we Check for it";
+const arr5 = str9.split(" ");
+for (var i = 0; i < arr5.length; i++) {
+  arr5[i] = arr5[i].charAt(0).toUpperCase() + arr5[i].slice(1);
+}
+const str10 = arr5.join(" ");
+console.log(str10);
+
+const str11 = "What is the difference between stopPropagation and preventDefault?";
+const arr6 = str11.split(" ");
+for (var i = 0; i < arr6.length; i++) {
+  arr6[i] = arr6[i].charAt(0).toUpperCase() + arr6[i].slice(1);
+}
+const str12 = arr6.join(" ");
+console.log(str12);
+
+const str13 = "Immutable State and Pure Functions";
+const arr7 = str13.split(" ");
+for (var i = 0; i < arr7.length; i++) {
+  arr7[i] = arr7[i].charAt(0).toUpperCase() + arr7[i].slice(1);
+}
+const str14 = arr7.join(" ");
+console.log(str14);
+
+const str15 = "what is the difference between == and ===?";
+const arr8 = str15.split(" ");
+for (var i = 0; i < arr8.length; i++) {
+  arr8[i] = arr8[i].charAt(0).toUpperCase() + arr8[i].slice(1);
+}
+const str16 = arr8.join(" ");
+console.log(str16);
+
+const str17 = "what is the difference between event capturing and bubbling?";
+const arr9 = str17.split(" ");
+for (var i = 0; i < arr9.length; i++) {
+  arr9[i] = arr9[i].charAt(0).toUpperCase() + arr9[i].slice(1);
+}
+const str18 = arr9.join(" ");
+console.log(str18);
+
+const str19 = "what is JSONP?";
+const arr10 = str19.split(" ");
+for (var i = 0; i < arr10.length; i++) {
+  arr10[i] = arr10[i].charAt(0).toUpperCase() + arr10[i].slice(1);
+}
+const str20 = arr10.join(" ");
+console.log(str20);
